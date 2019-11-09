794681e973341a70ee7dff59c21cbc94622d9e8c
Sara Elin <sedamyan@gmail.com>
2019-11-09 15:03:09 -0600

    added if else statements in js and checks for unvalid entries

--- script.js
+++ script.js
@@ -4,88 +4,62 @@ let lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 let numericChar = ["1","2","3","4","5","6","7","8","9","0"];
 let specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "="];
 let mixedChar = []
+let password = ""
 
-let charLength = prompt("Enter the amount of characters for your password between 8 - 28.");
+function generate(){
+
+let charLength = prompt("Enter the amount of characters for your password between 8 - 128.");
 
 if (charLength < 8) {
     alert("Please choose a number between 8-128.");
+    return;
 }
     else if (charLength > 128) {
         alert("Please choose a number between 8-128.");
+        return;
     }
 
+mixedChar = [];
 let upperYes = confirm ("Would you like to use an uppercase letter in your password?");
 if (upperYes) {
+    mixedChar.push.apply(mixedChar,upperChar)
 }
 
-confirm("Would you like to use a lowercase letter in your password?");
-confirm("Would you like to use a number in your password?");
-confirm("Would you like to use a special character in your password?");
+let lowerYes = confirm ("Would you like to use a lowercase letter in your password?");
+if (lowerYes) {
+    mixedChar.push.apply(mixedChar,lowerChar)
+}
+
+let numericYes = confirm("Would you like to use a number in your password?");
+if (numericYes) {
+    mixedChar.push.apply(mixedChar,numericChar)
+}
+
+let specialYes = confirm("Would you like to use a special character in your password?");
+    if (specialYes) {
+    mixedChar.push.apply(mixedChar,specialChar)
+}
+
 
 
     for (let i = 0; i < charLength; i++) {
-    // Randomly chooses a choice from the options array. 
-    let mixedChar = mixedChar[Math.floor(Math.random() * mixedChar.length)];
-
-
-
-
-alert("Your password is: " + )
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//generate random password
-function generate(){
-
-    //set password length/complexity
-    let complexity = document.getElementById("slider").value;
-
-    //possible password values
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-
-    let password = "";
-
-    //create for loop to choose password characters
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+        // Randomly chooses a choice from the options array. 
+        password = password + mixedChar[Math.floor(Math.random() * mixedChar.length)];    
     }
 
-    //add password to textbox/display area
-    document.getElementById("display").value = password;
+if (mixedChar.length === 0) {
+    alert ("Please select at least one character group.");
+}
+else{
 
+
+alert("Your password is: " + password)
+document.getElementById("display").value = password;
+}
 }
 
-   /* //set default length to 18
-    document.getElementById("Length").innerHTML = "Length: 18";
 
-    //function to set length based on slider position
-    document.getElementById("slider").oninput = function() {
-
-        if(document.getElementById ("slider").value > 0) {
-            document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
-        }
-        else{
-            document.getElementById("length").innnerHTML = "Length: 8";
-        }
-    }*/
-
-    //fucntion to copy to clipboard
+ //fucntion to copy to clipboard
     function copyPassword() {
         document.getElementById("display").select();
         document.execCommand("Copy");
